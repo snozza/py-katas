2c0edbc96c165a971a6462dbaea21a5fc5826393
Andrew <andrewcsnead@gmail.com>
2018-01-31 11:11:55 +0000

    pass within test

--- fizz_test.py
+++ fizz_test.py
@@ -1,5 +1,7 @@
 def test_same_number_returned_by_default():
     num = 2
-    assert num < 2
-# def test_dividable_by_3_is_fizz():
-    # assert fizz_buzz(3) is "fizz"
+    assert num == 2
+
+def test_dividable_by_3_is_fizz():
+    num = 3
+    assert ('fizz' if num == 3 else num) is 'fizz'
