0a0fd0e1bba0942e5cfa27c3b39c4f2b8cfed4ec
Andrew <andrewcsnead@gmail.com>
2018-01-31 11:24:25 +0000

    failing test for buzz

--- fizz_test.py
+++ fizz_test.py
@@ -8,7 +8,11 @@ def test_not_dividable_by_3_is_not_fizz():
 
 def test_dividable_by_5_is_buzz():
     num = 10 
-    assert 10 is True
+    assert (num == num) is True
+
+def test_not_dividable_by_5_is_not_buzz():
+    num = 9
+    assert (num / 5) is True 
 
 def is_fizz(num):
     return True if (num % 3) == 0 else False 
